721f9713029b47374914f96afd7e309bef4fccb5
Joe Wysocki <taillessbump@gmail.com>
2020-05-20 17:35:04 -0500

    Added gold and text for exp to next level

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let level = 0
 let expToNextLevel = 10
 let expGained = 0
 let addAmount = 1
-let multi = 1.5
+let multi = 2
 let automateAmt = 0
+let gold = 0
+let goldRate = 1
 
 setInterval(function(){
   for(let i = 0; i < automateAmt; i++){
@@ -13,16 +15,20 @@ setInterval(function(){
 
 $("#addExp").click(function() {
     expGained=expGained + addAmount
+    gold += goldRate
     
     
     if(expGained>=expToNextLevel){
         expToNextLevel=expToNextLevel*multi
         expGained=0
         level++
+        goldRate++
         $("#text1").text(level)
+        $("#nextLevel").text(expToNextLevel)
     }
 
     $("#expNum").text(expGained)
+    $("#gold").text(gold)
 }
 )
 
